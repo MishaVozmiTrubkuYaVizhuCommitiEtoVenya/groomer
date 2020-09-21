b30361c2bfde712fbb0609c95b4c65030a762557
Mikhail Shvedkov <mshvedkov@psystems.biz>
2020-09-21 06:53:25 +0300

    prepare for loaderio

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -56,7 +56,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60);
+            return Limit::perMinute(100000);
         });
     }
 }
